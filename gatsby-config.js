c2e8fd8562d04a7b63bd7918e9760bb642c66133
Daryl Roudebush <darylroudebush@Daryls-Mac-mini.local>
2020-02-11 22:14:01 -0500

    WebSite Title Change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `SitesReDone`,
+    description: `Have your outdated static website revitalize by SiteReDone using GatsbyJS.`,
+    author: `@SitesReDone`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
